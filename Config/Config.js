da6923362ece430458587d6d02c18b5835d15531
Lucas Gomez <lucasgomez675@gmail.com>
2023-11-14 17:22:11 -0300

    Esqueleto del Front

---
+++ Config/Config.js
@@ -0,0 +1,18 @@
+const config = {
+    
+    //Aca vamos a poner las conexiones a los microservicios :D
+    
+    // microservicioServicio : "https://localhost:7040",
+    // microservicioUsuario :  "https://localhost:7196",
+    // microservicioTransporte : "https://localhost:7155",
+    // microservicioReserva : "https://localhost:7061",
+    // microservicioDestino: "https://localhost:7018",
+    // microservicioViaje : "https://localhost:7198",
+
+
+    token : null,
+    idUsuario: null
+
+}
+
+export default config;
