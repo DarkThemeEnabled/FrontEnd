654eef2e3959df1968a421fb9a870a4b7e684ef9
Maarlopez <mariana.lopez.it@gmail.com>
2023-11-26 11:59:12 -0300

    Actualización de autenticación

--- Functions(js)/AuthFunctions/Login.js
+++ Functions(js)/AuthFunctions/Login.js
@@ -1,14 +1,51 @@
-const loginForm = document.querySelector('#loginForm')
-loginForm.addEventListener('submit', (e)=>{
-    e.preventDefault()
-    const email = document.querySelector('#email').value
-    const password = document.querySelector('#password').value
-
-    const Users = JSON.parse(localStorage.getItem('users')) || []
-    const validUser = Users.find(user => user.email === email && user.password === password)
-    if(!validUser){
-        return alert('Usuario y/o contraseña incorrectos')
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Verifica si el usuario ya está logueado y redirige a index.html
+    if(localStorage.getItem('userToken')) {
+        window.location.href = '../../index.html';
+        return;
     }
-    alert('Bienvenide ${validUser.nombre}')
-    window.location.href = 'index.html'    
-})
+
+    const loginForm = document.querySelector('#loginForm');
+    if(!loginForm) {
+        console.error('El formulario de login no se encontró en el DOM.');
+        return;
+    }
+
+    loginForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const emailInput = document.querySelector('#email');
+        const passwordInput = document.querySelector('#password');
+
+        if (!emailInput || !passwordInput) {
+            console.error('Los campos de email y/o contraseña no se encontraron en el DOM.');
+            return;
+        }
+
+        const email = emailInput.value;
+        const password = passwordInput.value;
+
+        // Aquí iría la lógica de autenticación, como un fetch a la API
+        // Por ejemplo:
+        fetch('https://localhost:7127/api/Usuario/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email, password })
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Credenciales incorrectas');
+            }
+            return response.json();
+        })
+        .then(data => {
+            localStorage.setItem('userToken', data.token);
+            window.location.href = '../../index.html';
+        })
+        .catch(error => {
+            alert(error.message);
+        });
+    });
+});
--- Functions(js)/AuthFunctions/Register.js
+++ Functions(js)/AuthFunctions/Register.js
@@ -1,23 +1,43 @@
-const signupForm = document.querySelector('#signupForm')
-signupForm.addEventListener('submit', (e) =>{
-    e.preventDefault()
-    const nombre = document.querySelector('#nombre').value
-    const apellido = document.querySelector('#apellido').value
-    const username = document.querySelector('#username').value
-    const email = document.querySelector('#email').value
-    const password = document.querySelector('#password').value
-    const photo = document.querySelector('#photo').value
+const signupForm = document.querySelector('#signupForm');
+signupForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    
+    const nombre = document.querySelector('#nombre').value;
+    const apellido = document.querySelector('#apellido').value;
+    const username = document.querySelector('#username').value;
+    const email = document.querySelector('#email').value;
+    const password = document.querySelector('#password').value;
+    const photo = document.querySelector('#photo').value; // Asegúrate de manejar correctamente la carga de archivos si 'photo' es un archivo
 
-    const Users = JSON.parse(localStorage.getItem('users')) || []
-    const isUserRegistered = Users.find(user => user.email === email)
+    // Aquí hacemos una petición POST al backend para registrar al usuario
+    try {
+        const response = await fetch('https://localhost:7127/api/Usuario/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                nombre: nombre,
+                apellido: apellido,
+                username: username,
+                email: email,
+                password: password,
+                photo: photo
+            })
+        });
 
-    if(isUserRegistered){
-        return alert('El usuario ya está registrado')
+        if (!response.ok) {
+            throw new Error('Error al registrar al usuario');
+        }
+
+        const data = await response.json();
+        alert('Registro exitoso');
+        window.location.href = 'login.html'; // Redirige al usuario a la página de inicio de sesión
+    } catch (error) {
+        if (error.message.includes('409')) {
+            alert('El usuario ya existe. Por favor, intenta con un email o nombre de usuario diferente.');
+          } else {
+            alert(error.message);
+        }
     }
-
-    Users.push({nombre: nombre, apellido: apellido, username: username, email: email, password: password, photo: photo})
-    localStorage.setItem('users', JSON.stringify(Users))
-    alert('Registro exitoso')
-    // redirección a login
-    window.location.href = 'login.html'
-})
+});
